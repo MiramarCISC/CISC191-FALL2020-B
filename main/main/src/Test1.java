39968212f57ad36037c97a3cfef42bd0bf47e609
Anita Cheung <anitacheun@gmail.com>
2020-11-04 04:01:30 -0800

    first test

---
+++ main/main/src/Test1.java
@@ -0,0 +1,18 @@
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.layout.GridPane;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.geometry.Insets;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+
+public class Test1 extends Application {
+    public static void main (String args[]) {
+
+    }
+}
